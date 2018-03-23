7fd94039d48cb9f7b28b03c52f38afb4a4559c9d
solsi <solsi.mob@gmail.com>
2018-03-23 14:34:53 +0100

    Prvi komit

---
+++ psolaj00lab1PIS/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace psolaj00lab1PIS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
