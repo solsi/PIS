3949fed737f9a2c02e22925c846594d0d94bc0ec
Filip Vidak <fvidak00@fesb.hr>
2018-03-23 14:51:36 +0100

    Update Program.cs
    Doda poziv

--- psolaj00lab1PIS/Program.cs
+++ psolaj00lab1PIS/Program.cs
@@ -10,6 +10,7 @@ namespace psolaj00lab1PIS
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World");
+            Console.WriteLine("Ajmo LoLa");
         }
     }
 }
